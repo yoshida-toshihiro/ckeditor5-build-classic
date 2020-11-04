beb5e3dac7cb916e14dd62102b9a4e25d3ffc167
yoshida-toshihiro <1657yoshida@stcousair.co.jp>
2020-11-04 15:05:19 +0900

    初回Build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -30,6 +30,8 @@ import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -56,7 +58,9 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+
+	Alignment,
 ];
 
 // Editor configuration.
